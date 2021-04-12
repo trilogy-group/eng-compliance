975748fa635f7eed25eee6762e682967bcfee838
Yousef Fadila <yousef.fadila@trilogy.com>
2021-04-12 14:24:38 +0200

    rename LambdaReporter, construct it on ComplianceChecker

--- src/reporters/CloudNotifierReporter.ts
+++ src/reporters/CloudNotifierReporter.ts
@@ -1,51 +1,52 @@
 import { HttpClient } from "@actions/http-client";
-import { throws } from "assert";
+
 import { CheckOptions } from "../check";
 import { Result } from "../ComplianceChecker";
 import { Product } from "../model/Product";
 import { Reporter } from "./Reporter";
 
 interface ResultRecord {
-  level: string,
-  rule?: string,
-  check?: string,
-  mandatory?: string,
-  result: string,
-  reason?: string
+  level: string;
+  rule?: string;
+  check?: string;
+  mandatory?: string;
+  result: string;
+  reason?: string;
 }
 
 interface RequestBody {
-  repo_owner?: string,
-  repo_name?: string,
-  start_time?: string,
-  end_time?: string,
-  result?: ResultRecord[]
+  repo_owner?: string;
+  repo_name?: string;
+  start_time?: string;
+  end_time?: string;
+  result?: ResultRecord[];
 }
 
-export class LambdaReporter extends Reporter {
+export class CloudNotifierReporter extends Reporter {
 
   private readonly uri: string;
-  private readonly keyHeader: any;
+  private readonly keyHeader: object;
   private readonly http: HttpClient;
   private body: RequestBody;
 
   constructor() {
     super();
-    if (process.env.INPUT_ENG_COMPLIANCE_LAMBDA_URI == null) throw new Error('INPUT_ENG_COMPLIANCE_LAMBDA_URI must be specified');
-    if (process.env.INPUT_ENG_COMPLIANCE_LAMBDA_KEY == null) throw new Error('INPUT_ENG_COMPLIANCE_LAMBDA_KEY must be specified');
+    if (process.env.INPUT_ENG_COMPLIANCE_CLOUD_NOTIFIER_URI == null || process.env.INPUT_ENG_COMPLIANCE_CLOUD_NOTIFIER_KEY == null)  {
+      throw new Error('INPUT_ENG_COMPLIANCE_LAMBDA_URI must be specified');
+    }
 
-    this.uri = process.env.INPUT_ENG_COMPLIANCE_LAMBDA_URI as string;
+    this.uri = process.env.INPUT_ENG_COMPLIANCE_CLOUD_NOTIFIER_URI as string;
     this.body = {};
     this.http = new HttpClient('eng-compliance-github');
 
-    let key = process.env.INPUT_ENG_COMPLIANCE_LAMBDA_KEY as string;
+    const key = process.env.INPUT_ENG_COMPLIANCE_CLOUD_NOTIFIER_KEY as string;
     this.keyHeader = {
       ['x-api-key']: key
     }
   }
 
   static enabled(): boolean {
-    return process.env.INPUT_ENG_COMPLIANCE_LAMBDA_URI != null && process.env.INPUT_ENG_COMPLIANCE_LAMBDA_KEY != null;
+    return process.env.INPUT_ENG_COMPLIANCE_CLOUD_NOTIFIER_URI != null && process.env.INPUT_ENG_COMPLIANCE_CLOUD_NOTIFIER_KEY != null;
   }
 
   startRun(product: Product) {
@@ -58,7 +59,7 @@ export class LambdaReporter extends Reporter {
   }
 
   reportCheck(ruleName: string, checkName: string, checkOptions: CheckOptions, outcome: Result, message?: string) {
-    let check_record: ResultRecord = {
+    let checkRecord: ResultRecord = {
       level: "check",
       rule: ruleName,
       check: checkName,
@@ -66,26 +67,26 @@ export class LambdaReporter extends Reporter {
       result: Result[outcome],
       reason: message?.replace(/[\/]/g, '_')
     }
-    this.body.result?.push(check_record);
+    this.body.result?.push(checkRecord);
   }
 
   reportRule(ruleName: string, outcome: Result) {
-    let rule_record: ResultRecord = {
+    let ruleRecord: ResultRecord = {
       level: "rule",
       rule: ruleName,
       result: Result[outcome]
     }
 
-    this.body.result?.push(rule_record);
+    this.body.result?.push(ruleRecord);
   }
 
   reportRun(product: Product, outcome: Result) {
-    let run_record: ResultRecord = {
+    let runRecord: ResultRecord = {
       level: "repo",
       result: Result[outcome]
     }
 
-    this.body.result?.push(run_record);
+    this.body.result?.push(runRecord);
 
     this.publishRecords();
   }
@@ -96,7 +97,7 @@ export class LambdaReporter extends Reporter {
     try {
       await this.http.postJson(this.uri, this.body, this.keyHeader);
     } catch (error) {
-      console.error('Error writing to Lambda', error);
+      console.error('Error writing to cloudNotifier', error);
     }
 
   }
