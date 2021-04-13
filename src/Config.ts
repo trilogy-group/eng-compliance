8d059e829be98e70ef1b14731d88189cb2ec5156
Yousef Fadila <yousef.fadila@trilogy.com>
2021-04-13 17:52:54 +0200

    naming conventions

--- src/Config.ts
+++ src/Config.ts
@@ -5,25 +5,25 @@ interface ConfigInterface {
 }
 
 export class Config {
-    private static cloud_notifier_uri?: string;
-    private static cloud_notifier_key?: string;
+    private static cloudNotifierURI?: string;
+    private static cloudNotifierKey?: string;
 
     static init() {
         const _configObjectString = process.env.ENG_COMPLIANCE_CONFIG_OBJECT || '{}';
         const configObject: ConfigInterface = JSON.parse(_configObjectString);
-        Config.cloud_notifier_uri = configObject.cloud_notifier_uri;
-        Config.cloud_notifier_key = configObject.cloud_notifier_key;
+        Config.cloudNotifierURI = configObject.cloud_notifier_uri;
+        Config.cloudNotifierKey = configObject.cloud_notifier_key;
     }
 
     static isCloudNotifierEnabled(): boolean {
-        return Config.cloud_notifier_uri != null && Config.cloud_notifier_key != null;
+        return Config.cloudNotifierURI != null && Config.cloudNotifierKey != null;
     }
 
     static getCloudNotifierURI(): string | undefined {
-        return Config.cloud_notifier_uri;
+        return Config.cloudNotifierURI;
     }
 
     static getCloudNotifierKey(): string | undefined {
-        return Config.cloud_notifier_key;
+        return Config.cloudNotifierKey;
     }
 }
